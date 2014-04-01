851313cd2ec21022a8f3ef731016f65933473caa
Alex Ehrnschwender <alexanderscott46@gmail.com>
2014-04-01 00:45:29 -0700

    initial commit

---
+++ config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+# javascripts_dir = "js"
+
+#output_style = :compressed
+#css_dir = "css_min"
+#generated_images_dir = "img/sprites"
+#http_generated_images_path = "img/sprites"
+#http_images_path = "/assets/img"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
